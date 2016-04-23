7b20231711f793019a3808293ab1803f2ecea42f
BuildTools <unconfigured@null.spigotmc.org>
2016-04-22 23:09:40 -0700

    Initial commit

---
+++ src/me/pyxled/abstractboard/AbstractScoreboard.java
@@ -0,0 +1,10 @@
+package me.pyxled.abstractboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Pyxled Development 2016 (c)
+ * File created: 04/22/2016
+ */
+public class AbstractScoreboard extends JavaPlugin {
+}
